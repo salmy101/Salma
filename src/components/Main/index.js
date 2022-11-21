12f9033e0b104f3f9156fb97a5cf2e0f49d11dd1
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-20 22:13:54 -0700

    set up app and start nav bar

---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+
+const main = function(){
+  return(
+    <div className='header'>
+      <h1>Main</h1>
+    </div>
+  )
+}
+
+export default main;

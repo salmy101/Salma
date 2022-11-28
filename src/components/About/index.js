3b01177e3a555d6a20483c6f364358f57de65e60
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-28 12:23:59 -0700

    formating, loaders, animation delays, layout, design

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,11 @@ export default function About() {
     <div className="container about-page">
       <div className="text-zone">
         <h1>hello</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus
+          egestas sed sed risus pretium. Nulla facilisi etiam
+        </p>
       </div>
     </div>
     <Loader type="ball-scale-ripple-multiple"/>

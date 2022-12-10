b6f8bde5b214874e1f7a71659998f0946094c6c1
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-12-09 17:06:57 -0700

    styling and creating portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,19 +1,41 @@
 import './index.scss'
 import React from 'react'
 import Loader from 'react-loaders'
-import portfolioData from "../../data/portfolio.json"
+import portfolioData from '../../data/portfolio.json'
 
 export default function Portfolio() {
-
-  const renderPortfolio = () => {
-    
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((project, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={project.cover}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+              <div className="content">
+                <p className="title">{project.title}</p>
+                <h4 className="description">{project.description}</h4>
+                <button
+                  className="btn"
+                  onClick={() => window.open(project.url)}
+                >
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
   return (
     <>
       <div className="container portfolio-page">
         <h1 className="page-title">PORTFOLIO</h1>
-
-        {/* <div>{renderPortfolio()}</div> */}
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       {/* <Loader type="ball-scale-ripple-multiple"/> */}
     </>
